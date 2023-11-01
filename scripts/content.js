2616889505b064711be73d1e60e72912c6c99e60
Tushar Gaurav <tushar.gaurav416@gmail.com>
2023-11-01 18:14:38 +0530

    updated

--- scripts/content.js
+++ scripts/content.js
@@ -33,6 +33,7 @@ function daysPassedSince(dateString) {
   return result;
 }
 
+// Get the release date from the product page in the "Additional Information" section
 let release_date = document
   .querySelector(
     "#productDetails_detailBullets_sections1 > tbody > tr:nth-child(4) > td"
@@ -41,6 +42,29 @@ let release_date = document
 
 let time_passed = daysPassedSince(release_date);
 
+if (time_passed === "Invalid") {
+  // If no release date is available, try to find the "Date First Available" element
+  const elements = document.querySelectorAll("span.a-text-bold");
+  // Loop through the found elements and find the specific one
+  elements.forEach((element) => {
+    if (element.textContent.includes("Date First Available")) {
+      // Navigate to the sibling span for the date
+      const dateElement = element.nextElementSibling;
+
+      if (dateElement) {
+        const date = dateElement.textContent.trim();
+        console.log("Date First Available:", date);
+        release_date = date;
+        // Further actions with the date content can be performed here
+      }
+    }
+  });
+
+  time_passed = daysPassedSince(release_date);
+} else {
+  release_date = release_date + ",";
+  time_passed = time_passed + " ago.";
+}
 // Adding the release date to the product pages
 const brElement = document.createElement("br");
 
@@ -48,19 +72,12 @@ const releaseDateSpan = document.createElement("span");
 releaseDateSpan.classList.add("a-text-bold");
 
 const releaseDateText = document.createTextNode("Release Date: ");
+
 releaseDateSpan.appendChild(releaseDateText);
 
 const actualDateSpan = document.createElement("span");
 actualDateSpan.classList.add("a-size-small");
 actualDateSpan.classList.add("a-color-secondary");
-
-if (time_passed === "Invalid") {
-  release_date = "Not Available";
-  time_passed = "";
-} else {
-  release_date = release_date + ",";
-  time_passed = time_passed + " ago.";
-}
 
 const actualDateText = document.createTextNode(
   `${release_date} ${time_passed}`
